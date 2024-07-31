e56bbd43b6dfedbab3a44dd4444eb196dfdd5916
wesam-shaker <hawawe560@gmail.com>
2024-07-31 14:36:07 +0300

    2024-7-31-1

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Smart Replay</title>
+    <title>Smart Replay ww</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
